e21618f084160fc59977a2a21be3760777da709a
Michael Lavers <kolanos@gmail.com>
2012-12-03 14:23:12 -0800

    Upping version to 0.1.0

--- culinary/__init__.py
+++ culinary/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 
 from . import (command, core, dir, file, group, misc, mode, option, package,
                process, python, repository, ssh, text, user)
